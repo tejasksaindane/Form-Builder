772fb12e3f36bdede5989012ded095742949cbad
tejasksaindane <tejassaindane99@gmail.com>
2024-03-10 13:48:57 +0530

    err

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({});
 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  publicRoutes: ["/"],
 };
